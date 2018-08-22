39de6bce43956ac6b3179b0ef1a67505719d9e83
Tatiana Veratti <Tatiana.Veratti@topbox.io>
2018-08-22 13:28:16 -0500

    update to react 16 and enzyme 3

--- test/mocha-setup.js
+++ test/mocha-setup.js
@@ -1,5 +1,9 @@
 import { jsdom } from 'jsdom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import styles from './fakes/styles';
+
+configure({ adapter: new Adapter() });
 
 // JSDOM
 const doc = jsdom('<!doctype html><html><body></body></html>');
