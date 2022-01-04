519b944b5b5e5ffed70d2975e18a080ae2be7ef5
Jason Hale <jason.hale@khoros.com>
2022-01-04 15:04:36 -0600

    eslint-config@2.0.1

--- helpers/assert.helper.js
+++ helpers/assert.helper.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 
+/* eslint-disable prefer-object-spread */
 module.exports = Object.assign({
     contains(parent, child) {
         assert.equal((-1 < String(parent).indexOf(child)), true);
